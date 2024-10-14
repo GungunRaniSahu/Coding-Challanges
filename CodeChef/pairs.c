910ec9174f031a7f0acf554a58e8fc4afeee94ba
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-10-14 21:32:37 +0530

    Feat: C program to check the oneful pairs

---
+++ CodeChef/pairs.c
@@ -0,0 +1,35 @@
+/*Oneful Pairs
+Chef defines a pair of positive integers (a,b) to be a Oneful Pair, if a+b+(a⋅b)= 111
+
+For example, (1,55) is a Oneful Pair, since 1+55+(1⋅55)=56+55=111. But (1,56) is not a Oneful Pair, since 1+56+(1⋅56)=57+56=113=111.
+
+Given two positive integers a and b, output Yes if they are a Oneful Pair. And No otherwise.
+
+Input Format
+The only line of input contains two space-separated integers a and b.
+
+Output Format
+Output Yes, if (a,b) form a Oneful Pair. Output No if they do not.
+You may print each character of Yes and No in uppercase or lowercase (for example, yes, yEs, Yes will be considered identical).*/
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int c , d;
+    
+    c = a+b;
+    d= a*b;
+
+    if(c+d==111)
+    {
+        printf("Yes\n");
+    }
+    
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}
