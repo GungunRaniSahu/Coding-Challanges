64ba3ed13d074ac94423c82b2ba303da2263541c
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-16 21:21:59 +0530

    Feat: C program to solve the negative number problem

---
+++ CodeChef/negative.c
@@ -0,0 +1,26 @@
+/*Task
+Write a program which does the following:
+
+Display the sum, product, and quotient of 2 integers - -50 & 40.
+Initialize the following variables in your program and use them in the computation
+Variable a and assign it the value -50
+Variable b and assign it the value 40
+Variables sum, product and quotient - assign the relevant values to them
+Note - sum, product and quotient need to be integers
+Output sum, product and quotient to the console on separate lines*/
+#include <stdio.h>
+
+int main()
+{
+
+  int a = -50;
+  int b = 40;
+  int sum = a + b;
+  int product = a * b;
+  int quotient = a / b;
+  printf("%d\n", sum);
+  printf("%d\n", product);
+  printf("%d", quotient);
+  
+  
+}
