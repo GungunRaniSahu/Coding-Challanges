5054fea0302bbf99b98a0c82fbf62624d99f2eeb
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-19 18:30:17 +0530

    Feat: Sum of squares

---
+++ CodeChef/Contest Questions/Q1.c
@@ -0,0 +1,21 @@
+/*Sum of squares of all numbers from 1 to a given number*/
+#include<stdio.h>
+void main()
+{
+  int num , sum =0 ;
+
+  scanf("%d" , &num);
+
+  if(num == 0)
+  {
+    return 1;
+  }
+
+  for(int i = 1 ; i < = num ; i++)
+    {
+      sum += i*i;
+    }
+
+  printf("%d" , sum);
+
+}
