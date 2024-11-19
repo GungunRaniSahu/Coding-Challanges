32982a8166cdf9ebc3d2fe50bf3d255db2d643f3
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-19 18:35:41 +0530

    Feat : Power of a number

---
+++ CodeChef/Contest Questions/Q3.c
@@ -0,0 +1,14 @@
+/*Power of one number to another*/
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int a , b , c;
+
+  scanf("%d" , &a);
+  scanf("%d" , &b);
+
+  s = pow(a,b);
+
+  printf("%d" , s);
+}
