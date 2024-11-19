9b73446fa75fdfc460f97d5991759817a0d7256f
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-19 18:32:57 +0530

    Feat: Skip the odd numbers and print the rest

---
+++ CodeChef/Contest Questions/Q2.c
@@ -0,0 +1,20 @@
+/*Skip Odd Number and print all the even number till the user inputs*/
+#include <stdio.h>
+void main()
+{
+  int n ;
+
+  scanf("%d", &n);
+
+  if(n == 0)
+  {
+    return 0;
+  }
+
+  for(int i = 2 ; i <= n ; i+=2)
+    {
+      printf("%d" , i);
+    }
+
+
+}
