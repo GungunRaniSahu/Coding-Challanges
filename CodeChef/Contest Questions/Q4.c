00ffdd506f1f128afc6504b33dc985f72cc133c8
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-19 19:06:30 +0530

    Feat: Ice cream and cones problem solution

---
+++ CodeChef/Contest Questions/Q4.c
@@ -0,0 +1,22 @@
+/*Icecream and Cone
+Chef has X cones and Y scoops of ice cream. Each ice cream cone requires exactly one cone and one scoop of ice cream.
+
+Your task is to determine the maximum number of ice cream cones Chef can make with the available ingredients.
+
+Input Format
+Input will contain two integers X and Y - the number of cones and ice cream scoops, respectively.
+Output Format
+Output the maximum number of ice cream cones Chef can make.*/
+#include <stdio.h>
+
+int main()
+{
+    int cones , scoops;
+    
+    scanf("%d" , &cones);
+    scanf("%d" , &scoops);
+    
+    printf("%d", scoops);
+
+    return 0;
+}
