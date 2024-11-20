a721c7fc270614352cddd308fe13592386102431
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-20 22:29:26 +0530

    Feat : Ai problem

---
+++ CodeChef/Contest Questions/Q5.c
@@ -0,0 +1,36 @@
+/*AI is Coming
+You are given an assignment from your institute.
+
+Each assignment has a difficulty value between 1 and 100 (both inclusive).
+AI can solve an assignment if and only if the difficulty of the assignment is less than or equal to 60.
+
+Your current assignment has difficulty X.
+Is AI capable of solving your assignment?
+Print "YES" if it is, and "NO" if it isn't (without the quotes).
+
+Input Format
+The first and only line of input will contain a single integer X, denoting the difficulty of your assignment.
+Output Format
+If AI is capable of solving your assignment output "YES", otherwise print "NO" (without quotes).
+Each character of the output may be printed in either uppercase or lowercase, i.e, the strings no, NO, No, and nO will all be treated as equivalent.*/
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+    scanf("%d" , &n);
+    
+    if(n<=60)
+    {
+        printf("YES\n");
+    }
+    
+    else
+    {
+        printf("NO\n");
+    }
+
+
+return 0;
+}
